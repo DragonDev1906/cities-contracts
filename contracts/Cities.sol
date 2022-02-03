dc722654fdd5792458889089a84a1cbbc0428487
Jens <jmw.1906@gmx.de>
2022-02-03 10:24:07 +0100

    Replace proof length check by hashing leaves and natspec comments

--- contracts/Cities.sol
+++ contracts/Cities.sol
@@ -30,35 +30,56 @@ uint256 constant TOKENID_MASK  = 0x00FFFFFF;
 uint256 constant TOKENID_SHIFT = 224;
 
 contract Cities is ERC721 {
-    bytes32 dataRootHash; // MerkleProof root used when minting to check if a city exists and if the data is correct
-    uint256 dataProofLength;
-    mapping(uint256 => CityData) public cityData; // Stores the city data after the token has been minted
+    /// @dev Store the MerkleTree root used for checking city existance and data integrity when minting. TODO: changing this to a constant should reduce the gas cost of _mintCity by about 2100 (gas cost of SLOAD since EIP-2929, Berlin)
+    bytes32 dataRootHash;
 
-    constructor(bytes32 _dataRootHash, uint256 _dataProofLength) ERC721("Cities", "CITY") {
+    /// @dev Store city data after the token has been minted
+    mapping(uint256 => CityData) public cityData;
+
+    /// @notice Constructor
+    /// @dev Stores _dataRootHash for use in _mintCity
+    /// @param _dataRootHash root of the MerkleTree used in _mintCity for chekcing existance and integrity of city data
+    constructor(bytes32 _dataRootHash) ERC721("Cities", "CITY") {
         dataRootHash = _dataRootHash;
-        dataProofLength = _dataProofLength;
     }
 
-    function _mintCity(address to, bytes32 _data, bytes32[] calldata proof) internal {
-        // Only allow leaf nodes to prevent someone from submitting an intermediate node of the merkle tree
-        require(proof.length == dataProofLength, "Invlid proof length");
-
-        // Make sure a city with this data exists
-        bool validProof = MerkleProof.verify(proof, dataRootHash, _data);
-        require(validProof, "Invalid city data or invalid tokenId");
+    /// @notice Mint a city ERC721 token using the tokenID stored in _data
+    /// @dev We don't need more than 2**24 different cities and the ID must be checked against the dataRootHash. To save gas we take it as one bytes32 parameter instead of using abi.encodePacked() before verifying the MerkleProof. There needs to be a mechanism for making sure intermediate nodes of the MerkleTree are not valid, otherwise someone could add a valid tokenID with garbage neighborhood information. This is done by hashing leaves of the MerkleTree (note: this would NOT be sufficient if _data would consist of 33-64 bytes). Alternatively proof.length could be checked, but this would require longer proofs for later entries. Paying about 80 additional gas for each city sounds better than paying about 1250 additional gas per increase in the proof size, even though the second only affects the later entries (note: if there are 2**x+1 cities the second option is probably cheaper, but in most cases the first one should be).
+    /// @param to receiver of the minted token
+    /// @param data 32 bytes containing city size and neighbor information that is checked using a MerkleProof to see if a city with this information exists.
+    /// @param proof MerkleTree proof for _data
+    function _mintCity(address to, bytes32 data, bytes32[] calldata proof) internal {
+        // Make sure a city with this data exists (note: this is only secure for len(_data)<=32  and len(_data)>64)
+        bool validProof = MerkleProof.verify(proof, dataRootHash, _keccak256(data));
+        require(validProof, "Invalid _data");
 
         // Parse and store city data
-        uint256 tokenId = (uint256(_data) >> TOKENID_SHIFT) & TOKENID_MASK;
-        cityData[tokenId].size = uint8(uint256(_data) >> 248) & 0x0F;
+        uint256 tokenId = (uint256(data) >> TOKENID_SHIFT) & TOKENID_MASK;
+        cityData[tokenId].size = uint8(uint256(data) >> 248) & 0x0F;
         cityData[tokenId].level = 0;
-        cityData[tokenId].neighborInfo = uint224(uint256(_data));
+        cityData[tokenId].neighborInfo = uint224(uint256(data));
 
         // Mint the token
         _safeMint(to, tokenId);
     }
 
-    function mintCity(address to, bytes32 _data, bytes32[] calldata proof) external {
-        _mintCity(to, _data, proof);
+    /// @notice Efficiently compute the hash of a single bytes32 value
+    /// @dev Without this we'd probably have to use abi.encodePacked, which costs a significant amount of gas (unless the optimizer recognizes what we want)
+    /// @param value The bytes32 value to be hashed
+    /// @return hash The hashed value
+    function _keccak256(bytes32 value) private pure returns (bytes32 hash) {
+        assembly {
+            mstore(0x00, value)
+            hash := keccak256(0x00, 0x20)
+        }
+    }
+
+    /// @notice Mint a (specific) city with the tokenId in data
+    /// @dev For now this is sufficient. TODO This function will need a limitation such that it can only be called by a CitySale contract that requires payment (Split due to Separation of Concerns)
+    /// @param data The data of the city to be minted (includes the tokenId)
+    /// @param proof Proof that a city with this data exists
+    function mintCity(bytes32 data, bytes32[] calldata proof) external {
+        _mintCity(msg.sender, data, proof);
     }
 
     function getCityData(uint256 tokenId) external view returns(uint8 _size, uint24 _level, uint224 _neighborInfo) {
--- test/Cities.js
+++ test/Cities.js
@@ -3,28 +3,43 @@ const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 
 describe("Cities contract", () => {
-    it("Allows minting", async () => {
+    it("Allows minting (manual test)", async () => {
         const [owner] = await ethers.getSigners();
         const CitiesContractFactory = await ethers.getContractFactory("Cities");
 
+        const index = 3;
         const elements = [
             "0x05001337F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF2F",
             "0x0F001338F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
             "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF01",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF02",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF03",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF04",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF05",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF06",
+            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF07",
+            "0x0000000000000000000000000000000000000000000000000000000000000000",
         ]
-        const merkleTree = new MerkleTree(elements, keccak256, { hashLeaves: false, sortPairs: true });
+        const merkleTree = new MerkleTree(elements, keccak256, { hashLeaves: true, sortPairs: true });
         const root = merkleTree.getHexRoot();
-        const proof = merkleTree.getHexProof(elements[0]);
+        const proof = merkleTree.getHexProof(keccak256(elements[index]));
         merkleTree.print()
 
         console.log("proof:", proof)
 
-        const CitiesContract = await CitiesContractFactory.deploy(root, 2);
+        const CitiesContract = await CitiesContractFactory.deploy(root);
         // await CitiesContract.mintCity(owner.address, "0x3faa5d346268279febf1b1db7d67b837e72fa0a0e88d8ccca3a0fb32e759f2e1", [])
-        await CitiesContract.mintCity(owner.address, elements[0], proof);
+        await CitiesContract.mintCity(elements[index], proof);
 
         // const data = await CitiesContract.getCityData(0x1337);
         const data = await CitiesContract.getCityData(0xaa5d34);
         console.log(data);
     })
 })
+
+// proofLength without require  : 98327
+// proofLength                  : 98319
+// hashLeaves=true              : 96298
+// proofLength with constant    : 96219
+// no checks                    : 96196

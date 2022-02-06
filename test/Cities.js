5d0d746e6998dedceb390d2e8d8746bf3fb55763
Jens <jmw.1906@gmx.de>
2022-02-06 21:25:38 +0100

    Create 4 automatic tests for minting

--- test/Cities.js
+++ test/Cities.js
@@ -3,37 +3,68 @@ const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 
 describe("Cities contract", () => {
-    it("Allows minting (manual test)", async () => {
-        const [owner, receiver] = await ethers.getSigners();
-        const CitiesContractFactory = await ethers.getContractFactory("Cities");
+    let accounts;
+    let owner, minter, receiver, normalUser;
+    const nonMintersVariations = [
+        ["the owner", 0],
+        ["the receiver", 2],
+        ["a normal user", 3]
+    ];
+    const allVariations = [
+        ["the owner", 0],
+        ["the minter", 1],
+        ["the receiver", 2],
+        ["a normal user", 3]
+    ];
 
-        const index = 3;
-        const elements = [
-            "0x05001337F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF2F",
-            "0x0F001338F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
-            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00",
-            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF01",
-            "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF02",
-        ]
-        const merkleTree = new MerkleTree(elements, keccak256, { hashLeaves: true, sortPairs: true });
+    const elements = [
+        "0x05001337F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF2F",
+        "0x0F001338F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+        "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00",
+        "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF01",
+        "0x0F001339F4FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF02",
+    ];
+    let merkleTree;
+    let citiesContract;
+    let citiesContractFactory;
+
+    getProof = (index) => {
+        return merkleTree.getHexProof(keccak256(elements[index]));
+    }
+
+    before(async () => {
+        // Accounts and variations
+        accounts = await ethers.getSigners();
+        [owner, minter, receiver, normalUser] = accounts
+
+        // Contract factory
+        citiesContractFactory = await ethers.getContractFactory("Cities");
+    })
+
+    beforeEach(async () => {
+        merkleTree = new MerkleTree(elements, keccak256, { hashLeaves: true, sortPairs: true });
         const root = merkleTree.getHexRoot();
-        const proof = merkleTree.getHexProof(keccak256(elements[index]));
-        merkleTree.print()
 
-        console.log("proof:", proof)
+        citiesContract = await citiesContractFactory.deploy(root);
+        await citiesContract.grantRole(await citiesContract.MINTER_ROLE(), minter.address)
+    })
 
-        const CitiesContract = await CitiesContractFactory.deploy(root);
-        await CitiesContract.grantRole(await CitiesContract.MINTER_ROLE(), owner.address)
-        await CitiesContract.mintCity(receiver.address, elements[index], proof);
+    describe("Minting", () => {
+        it("Allows the minter to mint existing cities", async () => {
+            await citiesContract.connect(minter).mintCity(receiver.address, elements[2], getProof(2))
+            const receiverBalance = await citiesContract.balanceOf(receiver.address)
+            expect(receiverBalance).to.equal(1)
+        })
 
-        // const data = await CitiesContract.getCityData(0x1337);
-        const data = await CitiesContract.getCityData(0xaa5d34);
-        console.log(data);
+        nonMintersVariations.forEach(([name, accountIndex]) => {
+            it(`Does not allow ${name} to mint existing cities`, async () => {
+                const signer = accounts[accountIndex]
+                await expect(
+                    citiesContract.connect(signer).mintCity(receiver.address, elements[2], getProof(2))
+                ).to.be.revertedWith("is missing role")
+                const receiverBalance = await citiesContract.balanceOf(receiver.address)
+                expect(receiverBalance).to.equal(0)
+            })
+        })
     })
 })
-
-// proofLength without require  : 98327
-// proofLength                  : 98319
-// hashLeaves=true              : 96298
-// proofLength with constant    : 96219
-// no checks                    : 96196
